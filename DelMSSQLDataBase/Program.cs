1e9f47a43ec2e2204ec0a005907edad4a3f849e4
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-07-10 18:50:16 +0300

    exclude demo base from delete

--- DelMSSQLDataBase/Program.cs
+++ DelMSSQLDataBase/Program.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.SqlServer.Management.Smo;
 using System;
+using System.IO;
 using System.Linq;
 //the Microsoft.SqlServer.SqlManagementObjects NuGet package
 namespace DelMSSQLDataBase {
     class Program {
         static void Main(string[] args) {
-
+            var namesToExcludeFile = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory+"namesToExclude.txt");
+            var namesToExclude = namesToExcludeFile.Split(';'); 
             var argumentValue = args[0].Substring(1);
             var sqlServer = new Server(@"(localdb)\mssqllocaldb");
+            
             if(argumentValue == "all") {
                 var count = sqlServer.Databases.Count;
                 for(int i = count - 1; i >= 0; i--) {
                     var db = sqlServer.Databases[i];
                     var dtDiff = DateTime.Today - db.CreateDate;
                     if(db.IsSystemObject || dtDiff.TotalDays < 15) {
+                        continue;
+                    }
+                    bool flag = false;
+                    var dbname = db.Name.ToLower();
+                    foreach(var nm in namesToExclude) {
+                        var exname = nm.ToLower();
+                        if(dbname.Contains(exname)) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(flag) {
                         continue;
                     }
                     DeleteDataBase(sqlServer, db.Name);
@@ -23,30 +38,12 @@ namespace DelMSSQLDataBase {
                     DeleteDataBase(sqlServer, argumentValue);
                 }
             }
-            //Console.ReadKey(); 
+           //Console.ReadKey();
         }
         static void DeleteDataBase(Server server, string dbName) {
             server.KillDatabase(dbName);
             Console.WriteLine("{0} deleted", dbName);
         }
-        //        void DirectDelete() {
 
-        //            string pathToSettingsFile = @"C:\MSSQLSettings.ini";
-        //            StreamReader sr = new StreamReader(pathToSettingsFile);
-        //            string st = sr.ReadToEnd();
-        //            sr.Close();
-
-        //            MsSqlConnector.GetSettingsFromTxt(st);
-        //            MsSqlConnector.MsServerName = @"(localdb)\mssqllocaldb";
-        //            MsSqlConnector.MsPass = "testDx55555";
-        //            MsSqlConnector.Open();
-        //            MsSqlConnector.MsDataBase = "master";
-        //            string st1 = @"ALTER DATABASE [dxMyXPo123] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE;
-        //DROP DATABASE [dxMyXPo123]";
-
-
-        //            MsSqlConnector.MakeNonQuery(st1);
-        //            MsSqlConnector.Close();
-        //        }
     }
 }
--- DelMSSQLDataBase/Properties/AssemblyInfo.cs
+++ DelMSSQLDataBase/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("18.2.14.2135")]
-[assembly: AssemblyVersion("18.2.14.2135")]
-[assembly: AssemblyFileVersion("18.2.14.2135")]
+// [assembly: AssemblyVersion("18.7.10.1849")]
+[assembly: AssemblyVersion("18.7.10.1849")]
+[assembly: AssemblyFileVersion("18.7.10.1849")]
